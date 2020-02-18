1672e1b61f116bc97d62e0c31e33599565315504
unknown <enniosinanaj96@gmail.com>
2020-02-18 12:18:01 -0500

    Adding the controller for the rest API. Exposing an endpoint to list all users.

---
+++ src/main/java/com/vacationplanner/controller/RestAPIController.java
@@ -0,0 +1,25 @@
+package com.vacationplanner.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vacationplanner.model.User;
+import com.vacationplanner.service.IUserService;
+
+@RestController
+@RequestMapping("/rest/api/v1")
+public class RestAPIController {
+	@Autowired
+    private IUserService userService;
+
+	@GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<User> findAll() {
+		return userService.findAll();
+	}
+	
+}
