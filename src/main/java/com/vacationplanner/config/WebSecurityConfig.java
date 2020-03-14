03ff4110c41a304b6f1b6333e58d8085b24c9c7b
dreameral <enniosinanaj96@gmail.com>
2020-03-14 19:52:50 +0100

    Add CORS support

--- src/main/java/com/vacationplanner/config/WebSecurityConfig.java
+++ src/main/java/com/vacationplanner/config/WebSecurityConfig.java
@@ -50,8 +50,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
+				.cors().and()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/login", "/register").permitAll().
 				// all other requests need to be authenticated
